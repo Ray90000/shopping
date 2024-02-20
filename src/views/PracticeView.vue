8f7d5a3ab74f4c1e1a714e123cc93dbb7d3b0402
Chuan jui <ray90130@gmail.com>
2024-02-20 18:20:32 +0800

    learn:ref,el

--- src/views/PracticeView.vue
+++ src/views/PracticeView.vue
@@ -11,21 +11,29 @@ export default {
       slideList: [dog_1, dog_2, dog_3],
       imageList: [
         {
-          name: '貓照片',
+          name: "貓照片",
           path: dog_1,
           is_cat: false,
         },
         {
-          name: '狗照片',
+          name: "狗照片",
           path: dog_2,
           is_cat: true,
         },
         {
-          name: '狗照片',
+          name: "狗照片",
           path: dog_3,
           is_cat: true,
         },
       ],
+      el: {
+        width: "N/A",
+        height: "N/A",
+      },
+      ref: {
+        width: "N/A",
+        height: "N/A",
+      },
     };
   },
   components: {
@@ -35,19 +43,48 @@ export default {
   setup() {
     return {};
   },
+  mounted() {
+    this.ref.width = this.$refs["ref-example"].clientWidth;
+    const divs = this.$refs["ref-example"];
+    if (divs && divs.length > 0) {
+    // 遍历所有 div 元素
+    divs.forEach(div => {
+      // 应用样式
+      div.style.color = 'red';
+    });
+  }
+    console.log(this.ref.width);
+    console.dir(this.$refs["ref-example"]);
+  },
 };
 </script>
 <template>
-  <swiper class="mySwiper border-[1px]" >
-    <swiper-slide v-for="(item, index) in slideList" :key="index" >
+  <!-- swiper -->
+  <swiper class="mySwiper border-[1px]">
+    <swiper-slide v-for="(item, index) in slideList" :key="index">
       <img :src="slideList[index]" alt="" />
     </swiper-slide>
   </swiper>
-  <div class="img-container m-[10px]" v-for="(image, index) in imageList" :key="index">
-    {{ image.name }} <br/>
-    {{ image.is_cat === true ? '有貓' : '無貓不秀' }} <br/>
-    <img :src="image.path" v-show="image.is_cat" alt="">
+  <!-- v-for v-show -->
+  <div
+    class="img-container m-[10px]"
+    v-for="(image, index) in imageList"
+    :key="index"
+    ref="ref-example"
+  >
+    {{ image.name }} <br />
+    {{ image.is_cat === true ? "有貓" : "無貓不秀" }} <br />
+    <img :src="image.path" v-show="image.is_cat" alt="" />
   </div>
+  <!-- $ref -->
+  <!-- <hr />
+  <br /> -->
+  <!-- <div>
+    el寬度:{{ el.width }}
+    <div ref="ref-example" class="bg-[red]">
+      ref寬度:{{ ref.width }} 
+    </div>
+  </div> -->
 </template>
 <style scoped>
 .swiper {
